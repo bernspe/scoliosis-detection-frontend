af8c9fdca82269dec21e9af0fe19c7e9c147e8a5
Peter Bernstein <bernspe@gmail.com>
2020-10-21 13:34:12 +0200

    Optimize Router / App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,48 @@
 <template>
   <v-app>
-    <v-app-bar app></v-app-bar>
-    <v-main>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense nav>
+        <v-list-item link to="/login">
+          <router-link to="/login"></router-link>
+          <v-list-item-icon>
+            <v-icon>mdi-book-multiple-outline</v-icon>
+          </v-list-item-icon>
 
-  <v-card>
-    <v-app-bar
-      color="deep-purple"
-      dark
-    >
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+          <v-list-item-content>
+            <v-list-item-title>Login</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/xrlist">
+          <router-link to="/xrlist"></router-link>
+          <v-list-item-icon>
+            <v-icon>mdi-book-multiple-outline</v-icon>
+          </v-list-item-icon>
 
+          <v-list-item-content>
+            <v-list-item-title>XR List</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/xrsubmit">
+          <router-link to="/xrsubmit"></router-link>
+          <v-list-item-icon>
+            <v-icon>mdi-radiology-box-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Submit New XRs</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+    </v-navigation-drawer>
+
+    <v-app-bar color="deep-purple" dark app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>XR Machine</v-toolbar-title>
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-8"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-                <router-link to="/">Home</router-link>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-                <router-link to="/login">Login</router-link>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
-
-          <template v-if="getterAuth">
-                      <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-book-multiple-outline</v-icon>
-                <router-link to="/xrlist">XR List</router-link>
-            </v-list-item-icon>
-            <v-list-item-title>XR List</v-list-item-title>
-          </v-list-item>
-
-            <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-radiology-box-outline</v-icon>
-                <router-link to="/xrsubmit">XR submit</router-link>
-            </v-list-item-icon>
-            <v-list-item-title>Submit New XRs</v-list-item-title>
-          </v-list-item>
-
-          </template>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <router-view/>
-
-
-        <v-container>
-            </v-container>
-  </v-card>
+    <v-main>
+    <router-view></router-view>
     </v-main>
   </v-app>
 </template>
